177e588ef74b3de198f4238bf33dd58efeac9331
Dipesh Das <77893779+Dipeshdas@users.noreply.github.com>
2023-09-02 21:52:00 +0530

    Add files via upload

---
+++ Quizzards/questions.js
@@ -0,0 +1,122 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "Which of the following attributes is used to add link to any element?",
+    answer: "href",
+    options: [
+      "link",
+      "href",
+      "ref",
+      "newref"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Markup Language",
+      "Hyper Text Preprocessor",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+  {
+    numb: 3,
+    question: "The property in CSS used to change the text color of an element is -",
+    answer: "color",
+    options: [
+      "bgcolor",
+      "color",
+      "background-color",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Which of the following is correct to write “Hello World” on the web page?",
+    answer: "document.write('Hello World')",
+    options: [
+      "print('Hello World')",
+      "document.write('Hello World')",
+      "response.write('Hello World')",
+      "None of These"
+    ],
+    
+  },
+
+  {
+    numb: 6,
+    question: "Which type of JavaScript language is ___",
+    answer: "Object-Based",
+    options: [
+      "Object-Oriented",
+      "Object-Based",
+      "Assembly-language",
+      "High-level"
+    ],
+
+  },
+
+  {
+    numb: 7,
+    question: "Which one of the following also known as Conditional Expression:?",
+    answer: "immediate if",
+    options: [
+      "Alternative to if-else",
+      "Switch statement",
+      "If-then-else statement",
+      "immediate if"
+    ],
+
+  },
+  {
+    numb: 8,
+    question: "The 'function' and 'var' are known as:",
+    answer: "Declaration statements",
+    options: [
+      "Keywords",
+      "Data types",
+      "Declaration statements",
+      "Prototypes"
+    ],
+
+  },
+  {
+    numb: 9,
+    question: "Which of the following is the correct syntax to make the background-color of all paragraph elements to yellow?",
+    answer: "p {background-color : yellow;}",
+    options: [
+      "p {background-color : yellow;}",
+      "p {background-color : #yellow;}",
+      "all {background-color : yellow;}",
+      "all p {background-color : #yellow;}"
+    ],
+
+  },
+  {
+    numb: 10,
+    question: "Which of the following tag is used for inserting the largest heading in HTML?",
+    answer: "h1",
+    options: [
+        "h3",
+        "h1",
+        "h5",
+        "h6"
+    ]
+  },
+  
+    
+];
